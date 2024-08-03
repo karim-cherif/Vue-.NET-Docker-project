00e6a3b4d6ed916694c81fe40658ada6fcb0d2ea
karim-cherif <abedelcarimc@gmail.com>
2024-08-03 19:25:51 +0100

    ci/cd fix

--- Back-End/obj/Debug/net8.0/Back-End.AssemblyInfo.cs
+++ Back-End/obj/Debug/net8.0/Back-End.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-End")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0af27402d6a3de3f5b06e4f5b5becbcd74f788")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824cbdb9a7bb15450de4fa69ec8a62aa96c12e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-End")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-End")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
