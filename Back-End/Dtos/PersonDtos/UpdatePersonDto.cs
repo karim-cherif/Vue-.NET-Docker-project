0f5d86a02afb573f74eb8e214712c4c45050589e
karim-cherif <abedelcarimc@gmail.com>
2025-05-21 21:42:44 +0100

    Add initial project files and configuration for Back-End
    - Created initial migration files for database setup.
    - Added user-specific settings for the solution.
    - Updated project package specifications with dependencies.
    - Included NuGet restore information for project management.
    - Generated static web assets removal file.

--- Back-End/Dtos/PersonDtos/UpdatePersonDto.cs
+++ Back-End/Dtos/PersonDtos/UpdatePersonDto.cs
@@ -1,9 +1,12 @@
-﻿namespace Back_End.Dtos.PersonDtos
+﻿
+
+namespace Back_End.Dtos.PersonDtos
 {
     public class UpdatePersonDto
     {
         public string Name { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        
         public DateTime DateOfBirth { get; set; } = DateTime.MinValue;
         public string Address { get; set; } = string.Empty;
     }
--- Back-End/Models/Person.cs
+++ Back-End/Models/Person.cs
@@ -1,8 +1,9 @@
-﻿namespace Back_End.Models
+﻿
+namespace Back_End.Models
 {
     public class Person
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public DateTime DateOfBirth { get; set; } = DateTime.MinValue;
